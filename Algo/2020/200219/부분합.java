87c62f457ea83b4e48bb54f6a0db81d2c7260e81
hasihime <46011077+hasihime@users.noreply.github.com>
2020-02-21 20:06:05 +0900

    200221

--- Algo/2020/200219/부분합.java
+++ Algo/2020/200219/부분합.java
@@ -7,54 +7,47 @@ import java.util.StringTokenizer;
 public class 부분합 {
 
 	private static int N;
-	private static int M;
+	private static long M;
 	private static int[] arr;
+
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		input();
-		Arrays.sort(arr);
-		//뒤에서부터 더하면서 온다?
 		go();
 	}
+
 	private static void go() {
-		int r=N-1;
-		int l=N=1;
-		int sum=0;
-		int cnt=0;
-		while (r>=0) {
-			if(arr[r]>M) {
-				r--;
-				continue;
-			}else {
+		int r = 0;
+		int l = 0;
+		long sum = 0;
+		int cnt = 100001;
+		while(r!=N) {
+			//sum이  M보다 작은 경우
+			if (sum<M) {
 				sum+=arr[r];
-				cnt++;
-				l=r-1;
-				int tsum=0;
-				while(l>=0) {
-					if (sum+arr[l]<=M) {
-						cnt++;
-						tsum+=arr[l];
-						sum+=arr[l];
-					}
-					if (sum==M) {
-						System.out.println(cnt);
-						return;
-					}
-					l--;
-				}
-				sum-=tsum;
+				r++;
+			}else  {
+				sum-=arr[l];
+				l++;
+				cnt=Math.min(cnt, (r-l)+1);
 			}
 		}
-		System.out.println(-1);
+		while(sum>=M) {
+			sum-=arr[l];
+			l++;
+			cnt=Math.min(cnt, r-l+1);
+		}
+		System.out.println(cnt==100001?0:cnt);
 	}
+
 	private static void input() throws NumberFormatException, IOException {
-		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st=new StringTokenizer(br.readLine()," ");
-		N=Integer.parseInt(st.nextToken());
-		M=Integer.parseInt(st.nextToken());
-		arr=new int[N];
-		st=new StringTokenizer(br.readLine()," ");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		N = Integer.parseInt(st.nextToken());
+		M = Long.parseLong(st.nextToken());
+		arr = new int[N];
+		st = new StringTokenizer(br.readLine(), " ");
 		for (int i = 0; i < N; i++) {
-			arr[i]=Integer.parseInt(st.nextToken());
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
 	}
 }
